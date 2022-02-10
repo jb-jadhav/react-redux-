a89500fce15920e9cf271c77362724e53ee79ff0
jb-jadhav <jibhaujadhav4@gmail.com>
2022-02-10 14:57:25 +0530

    react redux

---
+++ src/component/First.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';  
+
+class First extends React.Component
+{
+    render()
+    {
+        return(
+        // <h2> first component class</h2>
+        <></>
+        
+        );
+
+    }
+}
+export default First;
---
+++ src/component/Second.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';  
+class Second extends React.Component
+{
+  
+  
+  
+  render()
+  {
+      return(
+      <ul>ok</ul>
+
+      )
+  }  
+}
+export default Second
